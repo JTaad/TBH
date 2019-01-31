6950def3f6546a88a8a6f43c1b55106f4579829c
julien <julien@faakpaat.com>
2019-01-31 18:29:09 +0100

    imports vers require lambda

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,6 @@
-const sayHello = require('./modules')
-
-exports.handler = function handler(event, context, callback) {
+exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ hello: sayHello() }, null, 2),
-  })
-}
+    body: "Hello, World"
+  });
+};
