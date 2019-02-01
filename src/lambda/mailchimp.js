339fd58b3b72871ae2e7128d8ff2c0b3e03b59a6
julien <julien@faakpaat.com>
2019-02-01 10:18:55 +0100

    test mailchimp ES5

--- src/lambda/mailchimp.js
+++ src/lambda/mailchimp.js
@@ -1,26 +1,6 @@
-import Mailchimp from 'mailchimp-api-v3'
-import md5 from 'md5'
-require('dotenv').config()
-
-const mailchimp = new Mailchimp(process.env.MAILCHIMP_API)
-
-
-export async function handler (event, context, callback) {
-    console.log('ok')
-    const data = JSON.parse(event.body)
-    const email = data.email
-    const emailHash = md5(email)
-
-    await mailchimp.put(
-        `/lists/872c00aa85/membres/${emailHash}`,
-        {
-            email_address: email,
-            status: 'subscribed'
-        }
-    )
-
+exports.handler = function(event, context, callback) {
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({msg:  `📩 Votre e-mail ${email} a bien été enregistré` })
-    })
-  }
+      body: "Hello, World"
+    });
+  };
