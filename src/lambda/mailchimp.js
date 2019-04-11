5b8ae7d38fb6ebaf09301ce4364d6513aa0985b9
julien <julien@faakpaat.com>
2019-04-11 10:24:33 +0200

    mailchimp

---
+++ src/lambda/mailchimp.js
@@ -0,0 +1,26 @@
+var Mailchimp = require('mailchimp-api-v3')
+var md5 = require ('md5')
+require('dotenv').config()
+
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_API)
+
+
+exports.handler = function(event, context, callback) {
+    console.log('ok')
+    const data = JSON.parse(event.body)
+    const email = data.email
+    const emailHash = md5(email)
+
+    mailchimp.put(
+        `/lists/872c00aa85/membres/${emailHash}`,
+        {
+            email_address: email,
+            status: 'subscribed'
+        }
+    )
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({msg:  `📩 Votre e-mail ${email} a bien été enregistré` })
+    })
+  }
