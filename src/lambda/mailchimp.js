b3d3d3d28a5a693e5f5eac0d38b3c2199041cf3b
julien <julien@faakpaat.com>
2019-04-11 10:52:18 +0200

    mailchimp

--- src/lambda/mailchimp.js
+++ src/lambda/mailchimp.js
@@ -1,17 +1,17 @@
-import Mailchimp from 'mailchimp-api-v3'
-import md5 from 'md5'
+const Mailchimp = require('mailchimp-api-v3')
+const md5 = require('md5')
 require('dotenv').config()
 
 const mailchimp = new Mailchimp(process.env.MAILCHIMP_API)
 
 
-exports.handler = function(event, context, callback) {
+export async function handler (event, context, callback) {
     console.log('ok')
     const data = JSON.parse(event.body)
     const email = data.email
     const emailHash = md5(email)
 
-    mailchimp.put(
+    await mailchimp.put(
         `/lists/872c00aa85/membres/${emailHash}`,
         {
             email_address: email,
