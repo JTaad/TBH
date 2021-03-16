f2d5122f3c5214003b9b13150018571bf7741178
julien <julien@faakpaat.com>
2021-03-16 14:39:31 +0100

    ajout modal popup

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
     {
       resolve: `gatsby-source-instagram`,
       options: {
-        username: `tboutiquehotel`,
+        username: `8556131572`,
       },
     },
     {
