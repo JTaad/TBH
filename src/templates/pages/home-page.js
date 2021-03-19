3b49327d74c5bfd53bca87388b5b58bbe4cf14b1
julien <julien@faakpaat.com>
2021-03-19 17:00:24 +0100

    delete insta link

--- src/templates/pages/home-page.js
+++ src/templates/pages/home-page.js
@@ -67,6 +67,13 @@ export const pageQuery = graphql`
         node {
           timestamp
           id
+          localImage {
+            childImageSharp { 
+                fluid(maxWidth: 220, maxHeight: 210, quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
         }
       }
     }
